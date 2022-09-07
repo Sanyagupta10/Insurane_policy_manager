ae63b19c823d35f96134d463c2017d6d09237cb3
Shanya Gupta <96905963+Sanyagupta10@users.noreply.github.com>
2022-09-07 12:22:11 +0530

    Indent file

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -58,7 +58,6 @@ class PoliciesController < ApplicationController
       redirect_to login_path
     end
   end
-
   def update
     @policy = Policy.includes(:user, policytype: [:company]).find(params[:id])
     if @policy.update(policy_params)
@@ -68,7 +67,6 @@ class PoliciesController < ApplicationController
       render 'edit'
     end
   end
-
   def destroy
     @policy = Policy.includes(:user, policytype: [:company]).find(params[:id])
     @policy.destroy
