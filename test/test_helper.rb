bd20b1509e0c55a635541ab5a61aeae7b2236801
Sanya Gupta <sanyagupta103@gmail.com>
2022-09-18 16:30:13 +0530

    Fixed Some Rubocop errors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,28 +4,32 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Returns true if a test user is logged in.
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
+    # Returns true if a test user is logged in.
+    def logged_in?
+      !session[:user_id].nil?
+    end
 
-  # Log in as a particular user.
-  def log_in_as(user)
-    session[:user_id] = user.id
+    # Log in as a particular user.
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
   end
 end
 
-class ActionDispatch::IntegrationTest
-  # Log in as a particular user.
-  def log_in_as(user, password: 'password')
-    post login_path, params: { session: { email: user.email,
-                                          password: password } }
+module ActionDispatch
+  class IntegrationTest
+    # Log in as a particular user.
+    def log_in_as(user, password: 'password')
+      post login_path, params: { session: { email: user.email,
+                                            password: password } }
+    end
   end
 end
